cbbe817504005acaf66204387876a5a5958dfcf7
Saad5400 <sdbtwa@gmail.com>
2025-07-07 19:29:25 +0300

    search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+import BaseSearchBar from '@theme/SearchBar';
+
+export default function SearchBar() {
+    return (
+        <div className='mb-4'>
+            <BaseSearchBar />
+        </div>
+    )
+}
