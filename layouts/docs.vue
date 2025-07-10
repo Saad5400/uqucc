f7592e51171bb85eda2eeb5d6c3bf9057ff38da9
Saad5400 <sdbtwa@gmail.com>
2025-07-10 23:45:48 +0300

    ss

--- layouts/docs.vue
+++ layouts/docs.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ChevronLeft, ChevronDown, Star } from 'lucide-vue-next';
+
 const { data: items } = await useAsyncData('navigation', () =>
     queryCollectionNavigation('docs')
 );
@@ -6,18 +8,41 @@ const { data: items } = await useAsyncData('navigation', () =>
 
 <template>
     <SidebarProvider>
-        <Sidebar side="right">
+        <Sidebar side="right" variant="floating">
             <SidebarHeader />
             <SidebarContent>
                 <SidebarGroup>
                     <SidebarMenu>
-                        <SidebarMenuItem v-for="item in items || []" :key="item.stem" :to="`/${item.slug}`">
-                            <SidebarMenuButton as-child>
-                                <NuxtLink :to="item.path">
-                                    {{ item.title }}
-                                </NuxtLink>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
+                        <div class="contents" v-for="item in items || []" :key="item.stem" :to="`/${item.slug}`">
+                            <SidebarMenuItem v-if="!item.children || item.children.length === 0">
+                                <SidebarMenuButton as-child>
+                                    <NuxtLink :to="item.path">
+                                        {{ item.title }}
+                                    </NuxtLink>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                            <Collapsible v-else class="group/collapsible">
+                                <SidebarMenuItem>
+                                    <div class="flex items-center justify-between">
+                                        <SidebarMenuButton as-child>
+                                            <NuxtLink class="w-full" :to="item.path">
+                                                {{ item.title }}
+                                            </NuxtLink>
+                                        </SidebarMenuButton>
+                                        <CollapsibleTrigger asChild>
+                                            <SidebarMenuButton class="size-fit">
+                                                <ChevronLeft class="size-4" />
+                                            </SidebarMenuButton>
+                                        </CollapsibleTrigger>
+                                    </div>
+                                    <CollapsibleContent>
+                                        <SidebarMenuSub>
+                                            <SidebarMenuSubItem />
+                                        </SidebarMenuSub>
+                                    </CollapsibleContent>
+                                </SidebarMenuItem>
+                            </Collapsible>
+                        </div>
                     </SidebarMenu>
                 </SidebarGroup>
             </SidebarContent>
