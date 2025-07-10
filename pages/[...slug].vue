e7c219e028886060ba86ddd14fd61cecae1fade5
Saad5400 <sdbtwa@gmail.com>
2025-07-11 01:15:29 +0300

    docs are working!!

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,8 +2,13 @@
 definePageMeta({
   layout: 'docs',
 });
+
+const route = useRoute();
+const { data: page } = await useAsyncData(`pageData:${route.path}`, () => {
+  return queryCollection('docs').path(route.path).first();
+});
 </script>
 
 <template>
-    
+  <ContentRenderer v-if="page" :value="page" />
 </template>
