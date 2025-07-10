032461d91c2d1670d9885d92f3de7cbd8c3785df
Saad5400 <sdbtwa@gmail.com>
2025-07-10 21:24:52 +0300

    installed shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
@@ -9,5 +11,14 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
-  ]
+    'shadcn-nuxt',
+  ],
+
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
