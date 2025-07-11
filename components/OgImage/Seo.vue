919a3acede4b6945a1d7d94451d756a7883c4151
Saad5400 <sdbtwa@gmail.com>
2025-07-11 03:44:35 +0300

    style

--- components/OgImage/Seo.vue
+++ components/OgImage/Seo.vue
@@ -4,18 +4,26 @@ const props = defineProps<{
     description?: string;
 }>();
 
-const titleRender = h('div', { class: 'flex flex-row-reverse flex-wrap text-7xl pr-18' }, props.title?.split(' ').map(word => {
+const titleRender = h('div', { class: 'text-[#298287] flex flex-row-reverse flex-wrap text-7xl pr-18' }, props.title?.split(' ').map(word => {
     return h('span', { style: `width: ${word.length * 32}px` }, word);
 }))
 
-const descriptionRender = h('div', { class: 'flex flex-row-reverse flex-wrap text-5xl px-18' }, props.description?.split(' ').map(word => {
+const descriptionRender = h('div', { class: 'mt-10 flex flex-row-reverse flex-wrap text-5xl px-18' }, props.description?.split(' ').map(word => {
     return h('span', { style: `width: ${(word.length + 2) * 20}px`, class: 'py-1' }, word);
 }))
 </script>
 
 <template>
-    <div class="px-4 py-16 text-[#298287] bg-black size-full">
-        <!-- <component :is="titleRender" /> -->
+    <div
+    class="flex absolute top-0 right-[-100%]"
+    style="
+        width: 200%;
+        height: 200%;
+        background-image: radial-gradient(circle, rgba(41, 130, 135, 0.5) 0%, rgba(5, 5, 5, 0.3) 50%, (5, 5, 5, 0) 70%);
+    "
+    />
+    <div class="px-4 py-16 text-white/80">
+        <component :is="titleRender" />
         <component :is="descriptionRender" />
     </div>
 </template>
