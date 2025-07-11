354a486f550028e5d13626a84cdaa86e44578c59
Saad5400 <sdbtwa@gmail.com>
2025-07-11 16:42:02 +0300

    image style

--- components/OgImage/Seo.vue
+++ components/OgImage/Seo.vue
@@ -20,7 +20,7 @@ function divideLines(text: string) {
         return text.split('\n').map(line => line.trim());
     }
 
-    const maxLength = 45; // Maximum characters per line
+    const maxLength = 38; // Maximum characters per line
     const words = text.split(" ");
     const lines: string[] = [];
     let currentLine = "";
@@ -43,18 +43,18 @@ function divideLines(text: string) {
     <div class="flex absolute top-0 left-[-100%]" style="
             width: 200%;
             height: 200%;
-            background-image: radial-gradient(circle, rgba(41, 130, 135, 0.5) 0%, rgba(5, 5, 5, 0.3) 50%, (5, 5, 5, 0) 70%);
+            background-image: radial-gradient(circle, rgba(41, 130, 135, 0.5) 0%, rgba(14, 46, 60, 0.3) 50%, (5, 5, 5, 0) 70%);
         " />
     <div class="py-18 px-4 flex flex-col gap-4 items-end justify-start">
-        <div class="flex gap-8">
-            <img src="/favicon.svg" alt="Logo" class="w-16 h-16" />
+        <div class="flex items-center gap-8">
+            <img src="/favicon.svg" alt="Logo" class="size-14" />
             <h1 class="text-5xl font-bold m-0 whitespace-nowrap text-[#298287]">
                 {{ reverseText(title) }}
             </h1>
         </div>
 
         <hr />
-        <p v-for="line in reverseAndDivide(description)" class="text-4xl text-white/80 m-0 whitespace-nowrap">
+        <p v-for="line in reverseAndDivide(description)" class="text-5xl text-white/80 m-0 whitespace-nowrap">
             {{ line }}
         </p>
     </div>
