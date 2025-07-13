d92522ef6f6f7e8d3ddede800f2d39cedd72d221
Saad5400 <sdbtwa@gmail.com>
2025-07-13 21:10:36 +0300

    enable preview

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,45 +2,46 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxtjs/sitemap',
-    '@nuxtjs/robots',
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    'shadcn-nuxt',
-    '@nuxtjs/color-mode',
-    '@formkit/auto-animate/nuxt',
+    "@nuxtjs/sitemap",
+    "@nuxtjs/robots",
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@formkit/auto-animate/nuxt",
   ],
 
   site: {
-    url: 'http://uqucc.sb.sa',
-    name: 'دليل طالب كلية الحاسبات',
+    url: "http://uqucc.sb.sa",
+    name: "دليل طالب كلية الحاسبات",
+  },
+
+  content: {
+    preview: {
+      api: "https://api.nuxt.studio",
+    },
   },
 
   colorMode: {
-    classSuffix: '',
-    preference: 'dark',
+    classSuffix: "",
+    preference: "dark",
   },
 
   shadcn: {
-    prefix: '',
-    componentDir: './components/ui',
+    prefix: "",
+    componentDir: "./components/ui",
   },
 
-  css: [
-    '~/assets/css/tailwind.css',
-    '~/assets/css/typography.css',
-  ],
+  css: ["~/assets/css/tailwind.css", "~/assets/css/typography.css"],
 
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-})
+});
