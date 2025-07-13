28ce79e5e28de34e11520153440dbcabdf4f113c
Saad5400 <sdbtwa@gmail.com>
2025-07-13 22:11:38 +0300

    fuse search instead of mini

--- components/content/SearchBar.vue
+++ components/content/SearchBar.vue
@@ -1,55 +1,63 @@
 <script setup lang="ts">
-import MiniSearch from 'minisearch'
+import Fuse from "fuse.js";
 
-const query = ref('')
-const { data } = await useAsyncData('search', () => queryCollectionSearchSections('docs'))
+const query = ref("");
+const { data } = await useAsyncData("search-data", () =>
+  queryCollectionSearchSections("docs")
+);
 
-const miniSearch = new MiniSearch({
-    fields: ['title', 'content'],
-    storeFields: ['title', 'content'],
-    searchOptions: {
-        prefix: true,
-        fuzzy: 2,
-    },
-})
+const fuse = new Fuse(data.value, {
+  keys: ["title", "content"],
+});
 
-// Add data to the MiniSearch instance
-miniSearch.addAll(toValue(data.value) || [])
-const links = computed(() => miniSearch.search(toValue(query)))
-const uniqueLinks = computed(() => {
-    const seen = new Set<string>()
-    return links.value.filter(link => {
-        if (seen.has(link.title)) return false
-        seen.add(link.title)
-        return true
-    })
-})
+const results = computed(() => fuse.search(toValue(query)).slice(0, 10));
+const uniqueResults = computed(() => {
+  const seen = new Set();
+  return results.value.filter((result) => {
+    if (seen.has(result.item.title)) {
+      return false;
+    }
+    seen.add(result.item.title);
+    return true;
+  });
+});
 
 const open = ref(false);
 </script>
 
 <template>
-    <Dialog v-model:open="open">
-        <DialogTrigger as-child>
-            <Button variant="outline">
-                ابحث في الدليل
-            </Button>
-        </DialogTrigger>
-        <DialogContent>
-            <Input class="border-card-foreground/30" v-model="query" placeholder="بحث ..." />
-            <div class="flex flex-col gap-2 overflow-y-auto h-96">
-                <Button variant="ghost" v-for="link of uniqueLinks" :key="link.id" as-child @click="open = false"
-                    class="flex flex-col items-start whitespace-normal h-fit hover:!bg-card-foreground/10">
-                    <NuxtLink class="w-full" :to="link.id">
-                        <h5 class="font-semibold text-start">
-                            {{ link.title }}
-                        </h5>
-                        <p v-if="link.content" class="text-xs text-start text-foreground/70 line-clamp-2">
-                            {{ link.content }}
-                        </p>
-                    </NuxtLink>
-                </Button>
-            </div>
-        </DialogContent>
-    </Dialog>
+  <Dialog v-model:open="open">
+    <DialogTrigger as-child>
+      <Button variant="outline"> ابحث في الدليل </Button>
+    </DialogTrigger>
+    <DialogContent>
+      <Input
+        class="border-card-foreground/30"
+        v-model="query"
+        placeholder="بحث ..."
+      />
+      <div class="flex flex-col gap-2 overflow-y-auto h-96">
+        <Button
+          variant="ghost"
+          v-for="link of uniqueResults"
+          :key="link.item.id"
+          as-child
+          @click="open = false"
+          class="flex flex-col items-start whitespace-normal h-fit hover:!bg-card-foreground/10"
+        >
+          <NuxtLink class="w-full" :to="link.item.path">
+            <h5 class="font-semibold text-start">
+              {{ link.item.title }}
+            </h5>
+            <p
+              v-if="link.item.content"
+              class="text-xs text-start text-foreground/70 line-clamp-2"
+            >
+              {{ link.item.content }}
+            </p>
+          </NuxtLink>
+        </Button>
+      </div>
+    </DialogContent>
+  </Dialog>
 </template>
