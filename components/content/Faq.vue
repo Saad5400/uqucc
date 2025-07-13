7abb6c6bfe5046156329b39b6e004debb3bac56e
Saad5400 <sdbtwa@gmail.com>
2025-07-13 23:14:31 +0300

    better design

--- components/content/Faq.vue
+++ components/content/Faq.vue
@@ -3,14 +3,14 @@
 <template>
   <Collapsible>
     <CollapsibleTrigger
-      class="text-base border-2 whitespace-normal break-words h-fit w-full max-w-[30rem] text-start justify-between my-0"
+      class="text-base text-foreground/80 border-2 whitespace-normal break-words h-fit w-full max-w-[30rem] text-start justify-between my-0"
       as-child
     >
-      <Button variant="ghost">
+      <Button variant="secondary">
         <slot mdc-unwrap="p" />
       </Button>
     </CollapsibleTrigger>
-    <CollapsibleContent class="ms-6 ps-2 border-s-2 border-primary/50">
+    <CollapsibleContent class="ms-6 ps-2 border-primary/50">
       <slot name="content" mdc-unwrap="p, ul" />
     </CollapsibleContent>
   </Collapsible>
