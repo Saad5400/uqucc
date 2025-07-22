8ae0705d783234d9374cb22502a182ac1de8cb3f
Saad5400 <sdbtwa@gmail.com>
2025-07-22 20:32:46 +0300

    fixed target

--- components/content/prose/ProseA.vue
+++ components/content/prose/ProseA.vue
@@ -1,24 +1,37 @@
 <template>
-  <NuxtLink
-    :href="props.href"
-    :target="props.target"
-  >
+  <NuxtLink :href="props.href" :target="target">
     <slot />
   </NuxtLink>
 </template>
 
 <script setup lang="ts">
-import type { PropType } from 'vue'
+import type { PropType } from "vue";
 
 const props = defineProps({
   href: {
     type: String,
-    default: ''
+    default: "",
   },
   target: {
-    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+    type: String as PropType<
+      | "_blank"
+      | "_parent"
+      | "_self"
+      | "_top"
+      | (string & object)
+      | null
+      | undefined
+    >,
     default: undefined,
-    required: false
+    required: false,
+  },
+});
+
+const target = computed(() => {
+  if (props.href.startsWith("/documents") || props.href.startsWith("http")) {
+    return "_blank";
   }
-})
+
+  return props.target || "_self";
+});
 </script>
