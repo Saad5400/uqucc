9e836788c5876c717856fe5193a6e102ff5890ae
itsanas121 <161168604+itsanas121@users.noreply.github.com>
2025-08-13 04:24:46 +0300

    Update authors.ts

--- lib/authors.ts
+++ lib/authors.ts
@@ -4,14 +4,12 @@ export const authors = {
     },
     'oiixp1': {
         name: 'أثير الكناني',
-        url: 'https://x.com/oiixp1',
     },
     'Dina-x' :{
         name: 'دينا يوسف',
     },
     'BananArab': {
         name: 'بَنان عرب',
-        url: 'https://x.com/e_bananarab',
     },
     'fatimah-alqurashi-60b496330': {
         name: 'فاطمة القرشي',
@@ -19,11 +17,9 @@ export const authors = {
     },
     'mashae1_cs': {
         name: 'مشاعل شاكر',
-        url: 'https://x.com/mashae1_cs',
     },
     'o_hi_xl': {
         name: 'Ohoud',
-        url: 'https://x.com/o_hi_xl',
     },
     'حنين-cs-2a4313363': {
         name: 'حنين',
@@ -31,38 +27,29 @@ export const authors = {
     },
     'Kld-ai': {
         name: 'خالد الدمح',
-        url: 'https://x.com/kh9_ai',
     },
     'rito_4s': {
         name: 'ريتاج الصليمي',
-        url: 'https://x.com/rito_4s',
     },
     'muiopv': {
         name: 'يارا السلمي',
-        url: 'https://x.com/muiopv',
     },
     'Zartz14': {
         name: 'زياد إمام',
-        url: 'https://x.com/z_artz14',
     },
     'Evani': {
         name: 'محمد الشريف',
-        url: 'https://x.com/SWEevani',
     },
     'Nour': {
         name: 'نور عبدالعزيز',
-        url: 'https://x.com/78Nour_',
     },
     'khaled': {
         name: 'خالد النافع',
-        url: 'https://x.com/khal_x7',
     },
     'Shadi': {
         name: 'شادي رزق',
-        url: 'https://x.com/ShadiSWE',
     },
     'maziad': {
         name: 'مزيد العبدالعزيز',
-        url: 'https://x.com/maziad_140',
     }
 };
